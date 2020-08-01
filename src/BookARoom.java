a2e59db5c5240e213e587f3e56cce575ad92cc46
LiliyaKaplan <58580720+LiliyaKaplan@users.noreply.github.com>
2020-08-01 16:02:12 -0400

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,5 +1,7 @@
 public class BookARoom {
     public static void main(String[] args) {
         System.out.println("Some codes was written");
+        
+        // I added this line from GitHub
     }
 }
